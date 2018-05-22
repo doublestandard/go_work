f82b00b0d472ec14fbbedd939911d03f4af0f783
megumi-mochizuki <megumi.mochizuki@nhn-japan.com>
2018-05-22 10:29:37 +0900

    first commit

---
+++ zoo/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"zoo/animals"
+)
+
+func main() {
+	fmt.Println(AppName())
+	fmt.Println(animals.ElephantFeed())
+	fmt.Println(animals.RabbitFeed())
+	fmt.Println(animals.MonkeyFeed())
+}
